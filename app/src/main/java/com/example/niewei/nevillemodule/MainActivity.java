795a6b0e20052bd24bf1b2f10233eea65bfe67f2
niewei <bestneville@163.com>
2017-04-19 15:15:53 +0800

    第一次提交

---
+++ app/src/main/java/com/example/niewei/nevillemodule/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.niewei.nevillemodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
